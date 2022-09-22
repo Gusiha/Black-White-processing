2253afe9f718e86e34c4c06ed23a0d9f46f841a3
Тимур Милушев <Milushev.T@edu.rea.ru>
2022-09-22 21:06:19 +0300

    Have done: 1) Menu 2) Protect Input(bynary matrix)

--- Kursov/Program .cs
+++ Kursov/Program .cs
@@ -3,6 +3,18 @@
 using static MyLib.Sort;
 internal class Program
 {
+    // Вывести матрицу
+    public static void PrintBynary(int[,] arr, int Dim)
+    {
+        for (int i = 0; i < Dim; i++)
+        {
+            for (int j = 0; j < Dim; j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
     // Проверка на "серость"
     static bool Check(int Dimension, int[,] arr, int x, int y)
     {
@@ -19,7 +31,7 @@ internal class Program
         }
         return false;
     }
-    
+
     // Из десятичной системы в @base-систему счисления
     static int ConvertFromBase(int num, int @base)
     {
@@ -48,52 +60,147 @@ internal class Program
         return count;
     }
 
-    
+    static void Title()
+    {
+        Console.WriteLine("\t\t\tКУРСОВАЯ РАБОТА\n" +
+            "по дисциплине «Структуры и алгоритмы компьютерной обработки данных»\n" +
+            "на тему «Кратчайшие пути в бесконтурном графе»\n" +
+            "Выполнил студент 3 курса ВШКМиС\n" +
+            "Милушев Тимур Ильдусович\n");
+    }
+
+    static void MenuTitle()
+    {
+        Console.WriteLine(
+            "0. Ввести бинарную матрицу\n" +
+            "1. Ввести десятичную строку\n" +
+            "2. Обнуление матрицы\n" +
+            "3. Обнуление десятичной строки\n" +
+            "4. Вывести изображение\n" +
+            "5. Вывести десятичную строку\n"+
+            "6. Алгоритм [изображение]->[строка]\n"+
+            "7. Алгоритм [строка]->[изображение]\n"
+            );
+    }
+
     
 
     private static void Main(string[] args)
     {
-        // Инициализация
-        int[,] bynary_image =
-        {
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 1, 1, 1, 0, 0, 0},
-            {0 ,1, 1, 0, 1, 1, 1, 1},
-            {1 ,1, 0, 0, 1, 1, 1, 1},
-            {0 ,0, 1, 1, 0, 1, 0, 1},
-            {0 ,0, 1, 1, 0, 0, 1, 1},
-            {0 ,0, 1, 1, 1, 0, 1, 0},
-            {0 ,0, 1, 1, 1, 0, 0, 0}
-        };
-        
-        int Dim = FindDim(bynary_image);
+        Title();
+        int Dimension = 0;
+        int[,] bynary_image = null;
+        int MenuButton;
+        int[] decimal_image = null;
+        int temp = -1;
 
-        int[] decimal_image = new int[FindMaxSquares(bynary_image)];
-        int count = 0;
-        PrintBynary();
-        ToDecimalString(0, 0, Dim);
-        QuickSort(decimal_image, 0, decimal_image.Length-1);
-        Array.Reverse(decimal_image);
-        Array.Resize(ref decimal_image, Array.IndexOf(decimal_image, 0));
-        Print(decimal_image);
-        From5To10(decimal_image);
-        Print(decimal_image);
-        ToBynaryMatrix();
-        PrintBynary();
-        
+        //int[] decimal_image = new int[FindMaxSquares(bynary_image)];
 
-        void PrintBynary()
+        while (true)
         {
-            for (int i = 0; i < Math.Sqrt(bynary_image.Length); i++)
-                {
-                for (int j = 0; j < Math.Sqrt(bynary_image.Length); j++)
+            MenuTitle();
+            MenuButton = Int32.Parse(Console.ReadLine());
+            switch (MenuButton)
+            {
+                case 0:
                     {
-                    Console.Write($"{bynary_image[i,j]} ");
-                }
-                System.Console.WriteLine();
+                        Console.WriteLine("Введите размерность матрицы");
+                        Dimension = int.Parse(Console.ReadLine());
+                        bynary_image = new int[Dimension, Dimension];
+                        Console.WriteLine("Введите бинарное изображение");
+
+                        for (int i = 0; i < Dimension; i++)
+                        {
+                            for (int j = 0; j < Dimension; j++)
+                            {
+
+                                while (int.TryParse(Console.ReadLine(), out temp) == false || ! (temp == 0 || temp == 1))
+                                {
+                                    Console.WriteLine("Incorrect symbol. Input index again: ");
+                                }
+                                bynary_image[i, j] = temp;
+                                temp = -1;
+                            }
+
+                        }
+                        continue;
+                    }
+
+                case 1:
+                    {
+                        Console.WriteLine("Ввести десятичный вид изображения");
+                        if (Dimension == 0)
+                        {
+                            Console.WriteLine("Введите размерность будущего изображение");
+                            Dimension = int.Parse(Console.ReadLine());
+                        }
+                        decimal_image = new int[Dimension];
+
+                        for (int i = 0; i < Dimension; i++)
+                        {
+                            decimal_image[i] = int.Parse(Console.ReadLine());
+                        }
+                        continue;
+                    }
+                case 2:
+                    {
+                        Array.Clear(bynary_image);
+                        continue;
+                    }
+
+                case 3:
+                    {
+                        Array.Clear(decimal_image);
+                        continue;
+                    }
+                case 4:
+                    {
+                        PrintBynary(bynary_image, Dimension);
+                        continue;
+                    }
+                case 5:
+                    {
+                        Print(decimal_image);
+                        continue;
+                    }
+
+                default:
+                    break;
             }
+            break;
         }
-       
+        // Инициализация
+        //int[,] bynary_image =
+        //{
+        //    {0, 0, 0, 0, 0, 0, 0, 0},
+        //    {0, 0, 1, 1, 1, 0, 0, 0},
+        //    {0 ,1, 1, 0, 1, 1, 1, 1},
+        //    {1 ,1, 0, 0, 1, 1, 1, 1},
+        //    {0 ,0, 1, 1, 0, 1, 0, 1},
+        //    {0 ,0, 1, 1, 0, 0, 1, 1},
+        //    {0 ,0, 1, 1, 1, 0, 1, 0},
+        //    {0 ,0, 1, 1, 1, 0, 0, 0}
+        //};
+
+
+
+        
+
+        
+
+        int count = 0;
+        //PrintBynary(bynary_image, Dimension);
+        //ToDecimalString(0, 0, Dimension);
+        //QuickSort(decimal_image, 0, decimal_image.Length - 1);
+        //Array.Reverse(decimal_image);
+        //Array.Resize(ref decimal_image, Array.IndexOf(decimal_image, 0));
+        //Print(decimal_image);
+        //From5To10(decimal_image);
+        //Print(decimal_image);
+        //ToBynaryMatrix();
+        //PrintBynary(bynary_image, Dimension);
+        
+
         bool ToDecimalString(int i, int j, int Dimension, string Way = "")
 
         {
@@ -145,7 +252,7 @@ internal class Program
                 {
                     for (r = j; r < j + Dimension; r++)
                     {
-                        bynary_image[k,r] = 1;
+                        bynary_image[k, r] = 1;
                     }
                 }
             }
@@ -153,7 +260,7 @@ internal class Program
             {
                 k = Way % 5 - 1;
                 r = Dimension / 2;
-                FindBlackSquare(i+r*(k / 2), j+r*(k % 2), r, Way / 10);
+                FindBlackSquare(i + r * (k / 2), j + r * (k % 2), r, Way / 10);
             }
         }
 
@@ -190,7 +297,8 @@ internal class Program
             }
             Console.WriteLine();
         }
+
+
+
     }
-
-
 }
